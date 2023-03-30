1cda46c22bc933b8b692c8469f4fa7892a8ced22
qichang.dong <qcdong@metanovas.com>
2023-03-30 20:24:07 +0800

    u

---
+++ MsaServer/setup_databases-mmseqs.sh
@@ -0,0 +1,21 @@
+WORKDIR="${1:-$(pwd)}"
+cd "${WORKDIR}"
+
+if [ ! -f UNIREF30_READY ]; then
+  mmseqs tsv2exprofiledb "uniref30_2202" "uniref30_2202_db"
+  mmseqs createindex "uniref30_2202_db" tmp1 --remove-tmp-files 1
+  touch UNIREF30_READY
+fi
+
+if [ ! -f COLABDB_READY ]; then
+  mmseqs tsv2exprofiledb "colabfold_envdb_202108" "colabfold_envdb_202108_db"
+  # TODO: split memory value for createindex?
+  mmseqs createindex "colabfold_envdb_202108_db" tmp2 --remove-tmp-files 1
+  touch COLABDB_READY
+fi
+
+if [ ! -f PDB_READY ]; then
+  mmseqs createdb pdb70_220313.fasta.gz pdb70_220313
+  mmseqs createindex pdb70_220313 tmp3 --remove-tmp-files 1
+  touch PDB_READY
+fi
---
+++ MsaServer/start-local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# start the server in local mode
+# meaning both workers and server run from the same process
+./msa-server -local -config config.json
