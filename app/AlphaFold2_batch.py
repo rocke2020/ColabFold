3db6b91bffc9d9cf4293e0b13fb239590599764a
qichang.dong <qichang.dong@videt.cn>
2023-04-07 20:02:00 +0800

    u

--- app/AlphaFold2_batch.py
+++ app/AlphaFold2_batch.py
@@ -1,19 +1,15 @@
-import os, sys
-import re, random
 from pathlib import Path
-import json
-import pandas as pd
-import numpy as np
-from pandas import DataFrame
-from utils.log_util import logger
-from icecream import ic
-ic.configureOutput(includeContext=True, argToStringFunction=lambda _: str(_))
+import logging
 from sys import version_info
-python_version = f"{version_info.major}.{version_info.minor}"
-logger.info('python_version %s', python_version)
 from colabfold.batch import get_queries, run
 from colabfold.utils import setup_logging
 
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s %(lineno)d: %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+python_version = f"{version_info.major}.{version_info.minor}"
+logger.info('python_version %s', python_version)
 
 root_input_dir = Path('/mnt/sdc/af_input/pos_complex_in_one_seq')
 test_file = '1awr_C_I.fasta'
@@ -24,7 +20,7 @@ num_recycles = 9 #@param [1,3,6,12,24,48] {type:"raw"}
 stop_at_score = 100 #@param {type:"string"}
 #@markdown - early stop computing models once score > threshold (avg. plddt for "structures" and ptmscore for "complexes")
 use_custom_msa = False
-num_relax = 5
+num_relax = 0
 # use_amber = num_relax > 0, that's when num_relax > 0, use_amber becomes True auto.
 use_amber = False #@param {type:"boolean"}
 use_templates = False #@param {type:"boolean"}
@@ -32,12 +28,12 @@ do_not_overwrite_results = False #@param {type:"boolean"}
 zip_results = False #@param {type:"boolean"}
 
 
-single_file_input = 0
-if single_file_input:
+run_test_file = 0
+if run_test_file:
     input_dir = root_input_dir / test_file
 else:
     input_dir = root_input_dir
-result_dir = f'app/af_out/{Path(input_dir).stem}' #@param {type:"string"}
+result_dir = f'/mnt/adc/af_out/cf_{Path(input_dir).stem}' #@param {type:"string"}
 Path(result_dir).mkdir(exist_ok=1, parents=1)
 if 'logging_setup' not in globals():
     setup_logging(Path(result_dir).joinpath("log.log"))
