d9b531b8026a4b484b8701f482aee53cee842f47
qichang.dong <qichang.dong@videt.cn>
2023-10-20 14:46:17 +0800

    u

--- app/colabFold_batch_runner.py
+++ app/colabFold_batch_runner.py
@@ -11,18 +11,18 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s %(linen
 python_version = f"{version_info.major}.{version_info.minor}"
 logger.info('python_version %s', python_version)
 
-root_input_dir = Path('/mnt/sdc/af_input/pos_complex_in_one_seq')
+root_input_dir = Path('/mnt/sda/alphafold/af_input/pos_complex_in_one_seq')
 test_file = '1awr_C_I.fasta'
-default_data_dir = Path('/mnt/sdc/af_data')
+default_data_dir = Path('/mnt/sda/alphafold/af_data')
 msa_mode = "MMseqs2 (UniRef+Environmental)" #@param ["MMseqs2 (UniRef+Environmental)", "MMseqs2 (UniRef only)","single_sequence","custom"]
 num_models = 5 #@param [1,2,3,4,5] {type:"raw"}
 num_recycles = 9 #@param [1,3,6,12,24,48] {type:"raw"}
 stop_at_score = 100 #@param {type:"string"}
 #@markdown - early stop computing models once score > threshold (avg. plddt for "structures" and ptmscore for "complexes")
 use_custom_msa = False
-num_relax = 0
-# use_amber = num_relax > 0, that's when num_relax > 0, use_amber becomes True auto.
-use_amber = False #@param {type:"boolean"}
+num_relax = 3
+use_amber = num_relax > 0 # that's when num_relax > 0, use_amber becomes True auto.
+# use_amber = False #@param {type:"boolean"}
 use_templates = False #@param {type:"boolean"}
 do_not_overwrite_results = False #@param {type:"boolean"}
 zip_results = False #@param {type:"boolean"}
@@ -33,7 +33,7 @@ if run_test_file:
     input_dir = root_input_dir / test_file
 else:
     input_dir = root_input_dir
-result_dir = f'/mnt/adc/af_out/cf_{Path(input_dir).stem}' #@param {type:"string"}
+result_dir = f'/mnt/ada/alphafold/af_out/cf_{Path(input_dir).stem}' #@param {type:"string"}
 Path(result_dir).mkdir(exist_ok=1, parents=1)
 if 'logging_setup' not in globals():
     setup_logging(Path(result_dir).joinpath("log.log"))
--- run_colabfold.sh
+++ run_colabfold.sh
@@ -1,8 +1,7 @@
 [ ! -d app/zlog ] && mkdir app/zlog
-if [ $# -eq 2 ]; then echo 'args num is not 2'; exit 1; fi
 export CUDA_VISIBLE_DEVICES=$1
 # [ -f app/af_out/1awr_C_I/log.log ] && rm app/af_out/1awr_C_I/log.log
-[ -d app/af_out/1awr_C_I ] && sudo rm -rf app/af_out/1awr_C_I
-[ -d app/af_out/pos_complex_in_one_seq ] && sudo rm -rf app/af_out/pos_complex_in_one_seq
+# [ -d app/af_out/1awr_C_I ] && sudo rm -rf app/af_out/1awr_C_I
+# [ -d app/af_out/pos_complex_in_one_seq ] && sudo rm -rf app/af_out/pos_complex_in_one_seq
 nohup python run.py \
 > app/AlphaFold2_batch.log 2>&1 &
